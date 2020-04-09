92e2c93537b4ec3a6dd0712ce9b428d61a1592af
Dominik Madarász <zaklaus@outlook.com>
2020-04-09 20:08:09 +0200

    Clean up StdAfx.h

--- engine/StdAfx.h
+++ engine/StdAfx.h
@@ -1,22 +1,7 @@
-// stdafx.h : include file for standard system include files,
-//  or project specific include files that are used frequently, but
-//      are changed infrequently
-//
-
-#if !defined(AFX_STDAFX_H__498F612C_739E_4815_B26B_FD1899F5FEBB__INCLUDED_)
-#define AFX_STDAFX_H__498F612C_739E_4815_B26B_FD1899F5FEBB__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
 
-
-// Insert your headers here
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
 
 //{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_STDAFX_H__498F612C_739E_4815_B26B_FD1899F5FEBB__INCLUDED_)
