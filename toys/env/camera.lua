c8836e6e1591d2a2dcd0f002f013d1295a421b3d
Dominik Madarász <zaklaus@outlook.com>
2020-05-27 19:10:09 +0200

    Mirror demo fixes

---
+++ toys/env/camera.lua
@@ -0,0 +1,73 @@
+SPEED = 15.0
+SENSITIVITY = 0.15
+
+lookAt = nil
+
+camera = {
+	pos = Vector3(-2,0,-8),
+	fwd = Vector3(),
+	fwdl = Vector3(),
+	rhs = Vector3(),
+	vel = Vector3(),
+	angle = {0.48500002529471, 0},
+	heldControls = false
+}
+
+function updateCamera(dt)
+	handleInput(dt)
+
+	if GetCursorMode() == CURSORMODE_CENTERED then
+		mouseDelta = GetMouseDelta()
+		camera.angle[1] = camera.angle[1] + (mouseDelta[1] * dt * SENSITIVITY)
+		camera.angle[2] = camera.angle[2] - (mouseDelta[2] * dt * SENSITIVITY)
+
+		-- camera.angle[2] = math.clamp(-1.52, 1.55, camera.angle[2])
+	end
+
+	lookAt = Matrix():translate(camera.pos*-1)
+					  :rotate(-camera.angle[1], 0, 0)
+					  :rotate(0, camera.angle[2], 0)
+
+	camera.fwd = lookAt:col(3)
+	camera.rhs = lookAt:col(1)
+
+	camera.pos = camera.pos + camera.vel
+	camera.vel = camera.vel + camera.vel:neg()*0.10
+end
+
+function handleInput(dt)
+	local vel = Vector3()
+
+	if GetKey("w") then
+		vel = vel + Vector3(camera.fwd * dt * SPEED)
+	end
+
+	if GetKey("s") then
+		vel = vel + Vector3(camera.fwd * dt * SPEED):neg()
+	end
+
+	if GetKey("a") then
+		vel = vel + Vector3(camera.rhs * dt * SPEED):neg()
+	end
+
+	if GetKey("d") then
+		vel = vel + Vector3(camera.rhs * dt * SPEED)
+	end
+
+	if GetKey(KEY_SHIFT) then
+		vel = vel * 4
+	end
+
+	if vel:mag() ~= 0 then
+		camera.vel = camera.vel + (Vector3(vel) - camera.vel)*0.10
+		camera.heldControls = true
+	end
+end
+
+function euler(x,y,z)
+	return Matrix():rotate(0,0,z) * Matrix():rotate(0,y,0) * Matrix():rotate(x,0,0)
+end
+
+function lerp( a,b,t )
+	return a + (b - a)*t
+end
--- toys/env/init.lua
+++ toys/env/init.lua
@@ -1,5 +1,6 @@
 local test
 local testModel
+local monkey
 local sphere
 local skybox
 local time = 0
@@ -9,10 +10,13 @@ local rt
 local mat
 local rtMat
 
+dofile("camera.lua")
+
 function _init()
     testModel = Model("test.fbx")
     test = testModel:getRootNode()
     sphere = Model("sphere.fbx")
+    monkey = Model("monkey.fbx")
     mat = testModel:getMeshes()[1]:getMaterial(1)
 
     skybox = Model("skybox.fbx")
@@ -30,6 +34,9 @@ function _init()
     rt = RenderTarget(math.floor(res[1]/2), math.floor(res[2]/4), true)
     rtMat = Material()
     rtMat:setHandle(1, rt:getHandle())
+
+    ShowCursor(false)
+	SetCursorMode(CURSORMODE_CENTERED)
 end
 
 function _update(dt)
@@ -37,42 +44,45 @@ function _update(dt)
         ExitGame()
     end
 
+    if GetKeyDown(KEY_F2) then
+		ShowCursor(not IsCursorVisible())
+		SetCursorMode(1-GetCursorMode())
+    end
+
     time = time + dt
+    updateCamera(dt)
 end
 
 function _render()
     rt:bind()
     cam = Matrix():lookAt(
-        Vector(-2,0,-5),
         Vector(),
+        Vector(0,0,-1),
         Vector(0,1,0)
     ):rotate(math.sin(time), 0, 0)
-    camMat = Matrix():lookAt(
-        Vector(-2,0,-8),
-        Vector(),
-        Vector(0,1,0)
-    )
-    cam = camMat * cam
+    camMat = lookAt
     cam:bind(VIEW)
-    CameraPerspective(40, 0.1, 1000)
+    CameraPerspective(90, 0.1, 1000)
 
     EnableLighting(false)
 
     ClearScene(120,20,69)
     AmbientColor(16,16,16)
 
-    sphere:draw(Matrix():translate(2+math.cos(time)*1.2,0,-2))
+    sphere:draw(Matrix():translate(2+math.cos(time)*1.2,0,-4))
     skybox:draw(Matrix())
+    monkey:draw(Matrix():translate(camera.pos))
 
     ClearTarget()
     camMat:bind(VIEW)
     CameraPerspective(62, 0.1, 1000)
-    EnableLighting(true)
+    EnableLighting(false)
     ClearScene(20,20,69)
     testModel:getMeshes()[1]:setMaterial(0, rtMat)
     test:draw(Matrix():rotate(math.sin(time), 0, 0):scale(2,1,1))
-    sphere:draw(Matrix():translate(2 +math.cos(time)*1.2,0,-2))
+    sphere:draw(Matrix():translate(2 +math.cos(time)*1.2,0,-4))
     skybox:draw(Matrix())
+    --monkey:draw(Matrix():translate(camera.pos):translate(0,-1,0))
 end
 
 function _render2d()
