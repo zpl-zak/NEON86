a103e5dcde27d073eed531861cc6b5d0c3033ec4
Dominik Madarász <zaklaus@outlook.com>
2020-07-07 13:32:36 +0200

    Visual upgrade

--- demos/lines/world.lua
+++ demos/lines/world.lua
@@ -1,31 +1,59 @@
-local terrain, terrainMaterial, backdropModel, backdropMaterial
+local terrain, terrainMaterial, backdropModel, boundsMaterial, gradientMaterial
+local boundsMesh, gradientMesh, sunMesh, sunGlareMesh
+local bdMesh = {}
+
 world = {}
 local terrainShader
 
 function initWorld()
     terrain = Model("terrain.fbx")
-    backdropModel = Model("backdrop.fbx")
     terrainMaterial = Material("tile.png")
     terrainMaterial:setDiffuse(0x815192)
     terrainMaterial:setPower(120)
+    terrain:getMeshes()[1]:setMaterial(0, terrainMaterial)
 
-    backdropMaterial = Material("bounds.png")
-    backdropMaterial:setEmission(0xD546A6)
-    backdropMaterial:setDiffuse(0xD546A6)
-    backdropMaterial:alphaIsTransparency(true)
+    boundsMaterial = Material("bounds.png")
+    boundsMaterial:setEmission(0xD546A6)
+    boundsMaterial:setDiffuse(0xD546A6)
+    boundsMaterial:alphaIsTransparency(true)
 
+    gradientMaterial = Material("gradient.png")
+    gradientMaterial:setShaded(false)
+    
     world = cols.newWorld()
-
+    
     local meshNode = terrain:getRootNode():findNode("terrain")
     local mesh = meshNode:getMeshParts()[1][1]
-
+    
     for i=0,(WORLD_TILES[1]-1),1 do
         for j=0,(WORLD_TILES[2]-1),1 do
-        addTile(meshNode, mesh, Vector3(WORLD_SIZE*i,0,WORLD_SIZE*j))
+            addTile(meshNode, mesh, Vector3(WORLD_SIZE*i,0,WORLD_SIZE*j))
         end
     end
-
+    
     terrainShader = Effect("terrain.fx")
+
+    backdropModel = Model("backdrop.fbx")
+    backdropRoot = backdropModel:getRootNode()
+    boundsMesh = backdropRoot:findNode("bounds")
+    gradientMesh = backdropRoot:findNode("gradient")
+    sunMesh = backdropRoot:findNode("sun")
+    sunGlareMesh = backdropRoot:findNode("sunglare")
+    boundsMesh:getMeshes()[1]:setMaterial(0, boundsMaterial)
+    gradientMesh:getMeshes()[1]:setMaterial(0, gradientMaterial)
+    
+    bdMesh = backdropRoot:findNode("bd"):getNodes()
+
+    for _, bd in pairs(bdMesh) do
+        local dm = bd:getMeshes()[1]
+        local m = dm:getMaterial(1)
+        m:setShaded(false)
+    end
+
+    sunMesh:getMeshes()[1]:getMaterial(1):setShaded(false)
+    sunGlareMesh:getMeshes()[1]:getMaterial(1):setShaded(false)
+    sunGlareMesh:getMeshes()[1]:getMaterial(1):alphaTest(false)
+    sunGlareMesh:getMeshes()[1]:getMaterial(1):setAlphaRef(0)
 end
 
 function addTile(meshNode, mesh, pos)
@@ -42,15 +70,18 @@ function drawWorld()
     terrainShader:setVector3("campos", player.pos:neg())
     terrainShader:setLight("sun", light)
     terrainShader:commit()
-    BindTexture(0, terrainMaterial)
     for _, w in pairs(world.shapes) do
         terrain:draw(w.pos)
     end
-    BindTexture(0)
     terrainShader:endPass()
     terrainShader:done()
     
-    BindTexture(0, backdropMaterial)
-    backdropModel:draw(Matrix():scale(WORLD_TILES[1], WORLD_TILES[1], WORLD_TILES[1]))
-    BindTexture(0)
+    local wmat = Matrix():scale(WORLD_TILES[1], WORLD_TILES[1], WORLD_TILES[1])
+    gradientMesh:draw(wmat)
+    sunGlareMesh:draw(wmat)
+    sunMesh:draw(wmat)
+    for i=#bdMesh,1,-1 do
+        bdMesh[i]:draw(wmat)
+    end
+    boundsMesh:draw(wmat)
 end
