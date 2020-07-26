0a3bfbb405da45367942910ccde4fd3508d845a8
Dominik Madarász <zaklaus@outlook.com>
2020-07-26 02:37:20 +0200

    No trailing spaces

--- code/engine/LuaVertex.h
+++ code/engine/LuaVertex.h
@@ -8,7 +8,10 @@ INT vertex_new(lua_State* L)
 {
 	FLOAT x=0.0F, y=0.0F, z=0.0F;
 	FLOAT nx=0.0F, ny=0.0F, nz=0.0F;
+	FLOAT tx=0.0F, ty=0.0F, tz=0.0F;
+	FLOAT bx=0.0F, by=0.0F, bz=0.0F;
 	FLOAT su=0.0F, tv=0.0F;
+	FLOAT su2=0.0F, tv2=0.0F;
 	DWORD color = 0xFFFFFFFF;
 
 	if (lua_gettop(L) >= 3)
@@ -36,16 +39,49 @@ INT vertex_new(lua_State* L)
         nz = (FLOAT)luaL_checknumber(L, 9);
 	}
 
+	if (lua_gettop(L) >= 12)
+	{
+        tx = (FLOAT)luaL_checknumber(L, 10);
+        ty = (FLOAT)luaL_checknumber(L, 11);
+        tz = (FLOAT)luaL_checknumber(L, 12);
+	}
+
+	if (lua_gettop(L) >= 15)
+	{
+        bx = (FLOAT)luaL_checknumber(L, 13);
+        by = (FLOAT)luaL_checknumber(L, 14);
+        bz = (FLOAT)luaL_checknumber(L, 15);
+	}
+
+	if (lua_gettop(L) >= 17)
+	{
+        su2 = (FLOAT)luaL_checknumber(L, 16);
+        tv2 = (FLOAT)luaL_checknumber(L, 17);
+	}
+
 	VERTEX* vert = (VERTEX*)lua_newuserdata(L, sizeof(VERTEX));
 	ZeroMemory(vert, sizeof(VERTEX));
 	vert->x = x;
 	vert->y = y;
 	vert->z = z;
+
 	vert->su = su;
 	vert->tv = tv;
+
+	vert->su2 = su2;
+	vert->tv2 = tv2;
+
 	vert->nx = nx;
 	vert->ny = ny;
 	vert->nz = nz;
+
+	vert->tx = tx;
+	vert->ty = ty;
+	vert->tz = tz;
+
+	vert->bx = bx;
+	vert->by = by;
+	vert->bz = bz;
 	vert->color = color;
 
 	luaL_setmetatable(L, L_VERTEX);
@@ -55,18 +91,19 @@ INT vertex_new(lua_State* L)
 static INT vertex_get(lua_State* L)
 {
 	VERTEX* vert = (VERTEX*)luaL_checkudata(L, 1, L_VERTEX);
-	FLOAT arr[8] = { vert->x, vert->y, vert->z, vert->su, vert->tv, vert->nx, vert->ny, vert->nz };
+	FLOAT arr[] = { vert->x, vert->y, vert->z, vert->su, vert->tv, vert->nx, vert->ny, vert->nz, vert->tx, vert->ty, vert->tz, vert->bx, vert->by, vert->bz, vert->su2, vert->tv2 };
 	FLOAT col[4] = { (FLOAT)vert->r, (FLOAT)vert->g, (FLOAT)vert->b, (FLOAT)vert->a };
+	UINT arrLen = sizeof(arr)/sizeof(arr[0]);
 
 	lua_newtable(L);
-	for (UINT i=0; i<5; i++)
+	for (UINT i=0; i<arrLen; i++)
 	{
 		lua_pushinteger(L, i+1ULL);
 		lua_pushnumber(L, arr[i]);
 		lua_settable(L, -3);
 	}
 
-	lua_pushinteger(L, 6);
+	lua_pushinteger(L, arrLen+1);
 	lua_newtable(L);
 
 	for (UINT i=0; i<4; i++)
