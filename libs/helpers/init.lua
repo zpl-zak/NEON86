84c9e2e142796eb31751429981cee3a3f047358b
Dominik Madarász <zaklaus@outlook.com>
2020-07-27 13:56:58 +0200

    cleanup

--- libs/helpers/init.lua
+++ libs/helpers/init.lua
@@ -102,19 +102,19 @@ local function serializeTable(val, name, skipnewlines, depth)
    end
 
   if type(val) == "table" then
-      tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+    tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
 
-      for k, v in pairs(val) do
-          tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
-      end
+    for k, v in pairs(val) do
+      tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+    end
 
-      tmp = tmp .. string.rep(" ", depth) .. "}"
+    tmp = tmp .. string.rep(" ", depth) .. "}"
   elseif type(val) == "number" then
-      tmp = tmp .. tostring(val)
+    tmp = tmp .. tostring(val)
   elseif type(val) == "string" then
-      tmp = tmp .. string.format("%q", val)
+    tmp = tmp .. string.format("%q", val)
   elseif type(val) == "boolean" then
-      tmp = tmp .. (val and "true" or "false")
+    tmp = tmp .. (val and "true" or "false")
   else
       tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
   end
