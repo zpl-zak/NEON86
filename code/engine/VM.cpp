79e708845b4c8c13039af6676bfe02ded6146b1b
Dominik Madarász <zaklaus@outlook.com>
2020-10-12 10:59:53 +0200

    ReSharper cfg

--- code/engine/VM.cpp
+++ code/engine/VM.cpp
@@ -236,11 +236,11 @@ static const luaL_Reg loadedlibs[] = {
     {LUA_LOADLIBNAME, luaopen_package},
     {LUA_COLIBNAME, luaopen_coroutine},
     {LUA_TABLIBNAME, luaopen_table},
-    //{LUA_IOLIBNAME, luaopen_io},
-    //{LUA_OSLIBNAME, luaopen_os},
+    {LUA_IOLIBNAME, luaopen_io},
+    {LUA_OSLIBNAME, luaopen_os},
     {LUA_STRLIBNAME, luaopen_string},
     {LUA_MATHLIBNAME, luaopen_math},
-    //{LUA_UTF8LIBNAME, luaopen_utf8},
+    {LUA_UTF8LIBNAME, luaopen_utf8},
     {LUA_DBLIBNAME, luaopen_debug},
     #if defined(LUA_COMPAT_BITLIB)
 	{LUA_BITLIBNAME, luaopen_bit32},
@@ -250,14 +250,14 @@ static const luaL_Reg loadedlibs[] = {
 
 static void _lua_openlibs(lua_State* L)
 {
-    for (auto lib = loadedlibs; lib->func; lib++)
+    for (const auto* lib = loadedlibs; lib->func; lib++)
     {
         luaL_requiref(L, lib->name, lib->func, 1);
         lua_pop(L, 1);
     }
 
     static char path[MAX_PATH] = {0};
-    sprintf_s(path, MAX_PATH, "package.path = '%s/?/init.lua;libs/?/init.lua;%s/?.lua'",
+    sprintf_s(path, MAX_PATH, "package.path = '%s/?/init.lua;libs/?/init.lua;%s/?.lua;libs/?/?.lua'",
               FILESYSTEM->GetCanonicalGamePath(), FILESYSTEM->GetCanonicalGamePath());
     luaL_dostring(L, path);
     sprintf_s(path, MAX_PATH, "package.cpath = '%s/?.dll;libs/?.dll;libs/?/init.dll;libs/?/?.dll'",
